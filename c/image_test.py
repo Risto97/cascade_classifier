7c532f7e69b666b24d2f954a234ecbb6f46510ed
Risto Pejasinovic <risto.pejasinovic@gmail.com>
2018-11-19 21:36:22 +0100

    Fix last commit

--- c/image_test.py
+++ c/image_test.py
@@ -25,13 +25,11 @@ for img_index, img_fn in enumerate(filenames):
 
     detect = lib.detect
     detect.argtypes = [
-        ctl.ndpointer(np.uint8, flags='aligned, c_contiguous'),
+        ctl.ndpointer(np.uint8, flags='aligned, c_contiguous'), ctypes.c_int,
         ctypes.c_int,
-        ctypes.c_int,
-        ctl.ndpointer(np.uint16, flags='aligned, c_contiguous'),
-        ctypes.c_int,
-        ctl.ndpointer(np.float32, flags='aligned, c_contiguous'),
-        ctypes.c_float
+        ctl.ndpointer(np.uint16, flags='aligned, c_contiguous'), ctypes.c_int,
+        ctl.ndpointer(np.float32,
+                      flags='aligned, c_contiguous'), ctypes.c_float
     ]
 
     res = np.zeros(shape=(1000, 4), dtype='u2')
@@ -39,20 +37,23 @@ for img_index, img_fn in enumerate(filenames):
     res_num = 0
 
     start_time = time.time()
-
     res_num = detect(img, img.shape[0], img.shape[1], res, 1, hit, 1.2)
-
     stop_time = time.time()
     exec_time += stop_time - start_time
+
+    for i in range(res_num):
+        cv2.rectangle(img_clr, (res[i][0], res[i][1]),
+                      (res[i][0] + res[i][2], res[i][1] + res[i][3]),
+                      (0, 255, 0), 2)
 
     # np.set_printoptions(precision=3)
     # np.set_printoptions(suppress=True)
     # print(hit)
 
-    cv2.imwrite(f'../results/{img_index}.jpg', img_clr)
+    cv2.imwrite(f'../datasets/results/{img_index}.jpg', img_clr)
 
 print("--- TOTAl Time --------- %s s ---" % (exec_time))
-print("--- AVG Time --------- %s ms ---" % (exec_time/img_num*1000))
+print("--- AVG Time --------- %s ms ---" % (exec_time / img_num * 1000))
 print("---- FPS ------- %.2f  fps ----" % float(img_num / float(exec_time)))
 
 # cv2.imshow('img', img_clr)
