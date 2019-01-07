54fae03779b6be911f0167c2673124a6cc92b065
Risto Pejasinovic <risto.pejasinovic@gmail.com>
2019-01-07 23:32:45 +0100

    Pygears ii_gen DONE

--- pygears/gears/accum.py
+++ pygears/gears/accum.py
@@ -2,14 +2,8 @@ from pygears import gear
 from pygears.typing import Queue, Tuple, Integer, Uint
 
 @gear(svgen={'compile': True})
-async def accum(din: Queue['data_t', 2]) -> b'Queue[Uint[18], 1]':
+async def accum(din: Queue['data_t', 1]) -> b'Uint[18]':
     acc = Uint[18](0)
-    ret = Uint[18](0)
     async for (data, eot) in din:
-        if(eot[0] == 1):
-            ret = acc + int(data)
-            acc = Uint[18](0)
-            yield (ret, eot[1] * eot[0])
-        else:
-            acc = acc + int(data)
-            yield (acc, eot[1] * eot[0])
+        acc = acc + int(data)
+        yield acc
--- pygears/ii_gen.py
+++ pygears/ii_gen.py
@@ -1,72 +1,60 @@
 from pygears import gear, Intf
-from pygears.typing import Queue, Uint, Tuple, Int
-from pygears.svgen import svgen
-from pygears.util.print_hier import print_hier
+from pygears.typing import Queue, Uint
+from gears.fifo2 import fifo2
+from gears.accum import accum
+from pygears.common import ccat, add, shred
 
 from pygears.sim import sim
 from pygears.sim.modules import drv
 from pygears.sim.modules.verilator import SimVerilated
-from pygears.common import dreg, const, decoupler, shred, union_collapse
-
-# from pygears.common import add
-
-from gears.fifo2 import fifo2
-from gears.accum import accum
-from gears.add2 import add2
-
-from pygears.sim.extens.vcd import VCD
-from pygears.cookbook import priority_mux
-
-from pygears_view import main
-from functools import partial
 from pygears_view import PyGearsView
-from pygears import bind
-
-from pygears.common import ccat, czip, cart, fmap
-
-@gear
-def fill_void(din, fill):
-    return priority_mux(din, fill) \
-        | union_collapse
+from functools import partial
 
 din_t = Queue[Uint[8], 2]
 
+
+@gear
+def accum_wrap(din: din_t):
+    accum_in = ccat(din[0], din[1][0]) | Queue[din.dtype[0], 1]
+    accum_s = accum_in | accum
+    eot_s = din[1][1] * din[1][0]
+    dout_s = ccat(accum_s, eot_s) | Queue[accum_s.dtype, 1]
+
+    return dout_s
+
+
 @gear
 def ii_gen(din: din_t, *, frame_size=(25, 25)):
-    accum_s = din | accum
+    accum_s = din | accum_wrap
 
-    fifo_in = Intf(Queue[Uint[19], 1])
-    fifo_out = fifo_in | fifo2(
-        depth=32, preload=frame_size[1],
-        regout=False)
+    fifo_out = Intf(accum_s.dtype[0])
 
-    fifo_in |= add2(ccat(accum_s, fifo_out[0] | Uint[18])) | Queue[Uint[19], 1]
+    add_s = ccat(accum_s[0], fifo_out) | add
 
-    return fifo_in | Uint[18]
+    fifo_in = ccat(add_s, accum_s[1])
+
+    fifo_out |= fifo_in | fifo2(depth=32, preload=frame_size[1], regout=False)
+
+    dout_s = fifo_in
+
+    return dout_s
 
 
-frame_size = (5, 5)
+frame_size = (25, 25)
 seq = []
 for i in range(2):
     seq_y = []
     for y in range(frame_size[0]):
         seq_x = []
         for x in range(frame_size[1]):
-            seq_x.append(x+1)
+            seq_x.append(x + 1)
         seq_y.append(seq_x)
     seq.append(seq_y)
-print(seq)
 
-        # seq.append([list(range(1, frame_size[1] + 1))])
+ii_gen(
+    din=drv(t=din_t, seq=seq), frame_size=frame_size,
+    sim_cls=SimVerilated) | shred
 
-ii_gen(din=drv(t=din_t, seq=seq), frame_size=frame_size, sim_cls=SimVerilated) | shred
-
-sim(
-    outdir='./build',
-    # extens=[VCD])
+sim(outdir='./build',
     check_activity=True,
     extens=[partial(PyGearsView, live=True, reload=True)])
-
-print()
-print_hier()
-print()
