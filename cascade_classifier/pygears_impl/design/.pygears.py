28782ba7ebecca9064a38e6a31fa0e7606438d19
Risto Pejasinovic <risto.pejasinovic@gmail.com>
2019-07-20 16:22:32 +0200

    svgen -> hdl

--- cascade_classifier/pygears_impl/design/.pygears.py
+++ cascade_classifier/pygears_impl/design/.pygears.py
@@ -3,6 +3,6 @@ from pygears.conf.registry import registry, bind
 
 svlib_dir = os.path.join(
     os.path.dirname(__file__), 'gears', 'svlib')
-registry('svgen/sv_paths').append(svlib_dir)
+registry('hdl/include_paths').append(svlib_dir)
 
 bind('hdl/debug_intfs', ['*'])
